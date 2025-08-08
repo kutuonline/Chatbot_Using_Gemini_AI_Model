4117e1bf9ef4a9756a2575a091b560b54d3eab52
Andriansyah Doni <andrean.devz@gmail.com>
2025-08-08 16:38:15 +0700

    Update chatbot_app.py

--- chatbot_app.py
+++ chatbot_app.py
@@ -1,10 +1,10 @@
-# import dotenv
-# from dotenv import load_dotenv
+import dotenv
+from dotenv import load_dotenv
 import streamlit as st
 import os
 import google.generativeai as genai
 
-# load_dotenv()
+load_dotenv()
 
 # Initialize Gemini-Pro 
 # genai.configure(api_key="AIzaSyC9ZvfAwrmyoLYbA16gncvmQf20GBOrEbo")
@@ -50,6 +50,7 @@ if prompt := st.chat_input("Ask anything"):
       st.markdown(response.text)
     except ValueError as e:
       st.error(f"Error accessing response text: {e}")
+
 
 
 
