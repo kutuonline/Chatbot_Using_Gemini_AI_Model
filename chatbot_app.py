eab0501282d4ecad0e0a78a0eebfc1c3b51ed1ea
Andriansyah Doni <andrean.devz@gmail.com>
2025-08-08 16:01:40 +0700

    Update chatbot_app.py

--- chatbot_app.py
+++ chatbot_app.py
@@ -11,7 +11,7 @@ load_dotenv()
 # genai.configure(api_key=os.getenv("AIzaSyC9ZvfAwrmyoLYbA16gncvmQf20GBOrEbo"))
 
 # get_api_key = os.getenv("AIzaSyC9ZvfAwrmyoLYbA16gncvmQf20GBOrEbo")
-get_api_key = Get_API_Key
+get_api_key = "AIzaSyC9ZvfAwrmyoLYbA16gncvmQf20GBOrEbo"
 genai.configure(api_key = get_api_key)
 
 model = genai.GenerativeModel("gemini-2.5-pro")
@@ -50,6 +50,7 @@ if prompt := st.chat_input("Ask anything"):
       st.markdown(response.text)
     except ValueError as e:
       st.error(f"Error accessing response text: {e}")
+
 
 
 
